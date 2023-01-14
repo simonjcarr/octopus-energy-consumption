ac1f193eebe9f20d6975f8bca98beb4dc025d0a4
simonjcarr <simonjcarr@gmail.com>
2023-01-14 22:26:34 +0000

    debug vercel URL

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 const API_URL = process.env.APP_API_BASE
-
+const VERCEL_URL = process.env.VERCEL_URL
 function page() {
   return (
     <div>
       <div>page</div>
       <div>API URL {API_URL}</div>
+      <div>VERCEL URL {VERCEL_URL}</div>
     </div>
   )
 }
